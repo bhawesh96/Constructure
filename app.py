53bd35f50519e51350e416e1b55dad87dcfc978a
bhawesh96 <=>
2017-09-16 17:42:15 +0530

    initial setup

---
+++ app.py
@@ -0,0 +1,25 @@
+import traceback, warnings
+warnings.filterwarnings("ignore")
+
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+# MySQL configurations
+
+@app.route('/home')
+@app.route('/')
+def main():
+    return render_template('home.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()    
+    return redirect('/')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    app.run(debug=False,port=5005,use_evalex=False)
